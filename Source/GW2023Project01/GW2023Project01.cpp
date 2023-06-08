43dbaef5e39665b0b773a9cb85cfc9d636e45637
niitsu <yniitsu@air.ocn.ne.jp>
2023-06-09 08:58:27 +0900

    プロジェクト作り直した

--- Source/GW2023Project01/GW2023Project01.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GW2023Project01.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GW2023Project01, "GW2023Project01" );
